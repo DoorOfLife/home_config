19c55ab5320f50e47a5b374eb354b81e330c1ffe
Root <mrdoor@protonmail.ch>
2020-12-22 03:51:15 +0100

    Adding secondbar to polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,7 @@
 # Terminate already running bar instances
 killall -q polybar
 polybar mainbar 2>&1 | tee -a /var/log/mainbar.log & disown
+polybar secondbar 2>&1 | tee -a /var/log/secondbar.log & disown
 
 echo "Bar(s) launched..."
 
